58e772f7a550d7468110f7f3d8f9c81d90fea7c4
nzjl94 <nzjl94@gmail.com>
2024-04-23 23:30:39 +0200

    Testing GitHub -1

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,12 +1,9 @@
 import HEADER from "../ui/HEADER"
-
 export default () => {
-
   return <div className="pt-[100px]">
       <HEADER 
           title="Get in Touch with Estatein" 
           content="Welcome to Estatein's Contact Us page. We're here to assist you with any inquiries, requests, or feedback you may have. Whether you're looking to buy or sell a property, explore investment opportunities, or simply want to connect, we're just a message away. Reach out to us, and let's start a conversation." 
       />
   </div>
-  
 };
